38269be66faca0a4d5c6e8fb0953ee5987f443e2
Aroes <nep@nep.me>
2024-10-11 21:55:31 +0900

    Amphibians - init and add dependencies

---
+++ Amphibians/README.md
@@ -0,0 +1,6 @@
+# 练习：构建两栖动物应用
+
+两栖动物 JSON 数据托管在以下网址
+https://android-kotlin-fun-mars-server.appspot.com/amphibians
+
+![Preview](./preview.png)
--- Amphibians/app/src/main/java/com/example/amphibians/MainActivity.kt
+++ Amphibians/app/src/main/java/com/example/amphibians/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import com.example.amphibians.ui.screens.HomeScreen
 import com.example.amphibians.ui.theme.AmphibiansTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,9 +14,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AmphibiansTheme {
-
+                AmphibiansApp()
             }
         }
     }
 }
 
+@Composable
+fun AmphibiansApp(){
+    HomeScreen()
+}
