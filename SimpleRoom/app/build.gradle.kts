e809f7073955575f26e1949353d0cf1ffb111cac
Aroes <nep@nep.me>
2024-10-13 14:15:50 +0900

    RoomSimple - add dependencies

--- SimpleRoom/app/build.gradle.kts
+++ SimpleRoom/app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.google.ksp)
+    alias(libs.plugins.android.hilt)
 }
 
 android {
@@ -49,6 +51,10 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.bundles.hilt)
+    implementation(libs.bundles.room)
+    ksp(libs.bundles.hilt.ksp)
+    ksp(libs.androidx.room.compiler)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
