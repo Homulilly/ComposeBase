92656675826aff8866833c6d93b6c5719b987802
Aroes <nep@nep.me>
2024-10-13 15:01:47 +0900

    RoomSimple - room add database

---
+++ SimpleRoom/app/src/main/java/com/example/simpleroom/data/UserDao.kt
@@ -0,0 +1,22 @@
+package com.example.simpleroom.data
+
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+interface UserDao {
+    @Insert(onConflict = OnConflictStrategy.Companion.REPLACE)
+    suspend fun insertUser(user: User)
+
+    @Update
+    suspend fun updateUser(user: User)
+
+    @Delete
+    suspend fun deleteUser(user: User)
+
+    @Query("SELECT * FROM users")
+    fun getAllUsers(): Flow<List<User>>
+}
